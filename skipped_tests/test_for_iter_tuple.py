c873d7f70d6fede95c20abbc8d3665bb839aa23f
Lu, Kuang-Chen <lukuangchen1024@gmail.com>
2021-12-27 20:47:53 -0500

    I still do not want to support for-loop, too time consuming. It relies on iterators

---
+++ skipped_tests/test_for_iter_tuple.py
@@ -0,0 +1,15 @@
+# Reason: Code hitted some skipped words
+def test_for_iter_tuple(self):
+    codestr = """
+        from typing import List
+        def f(n: int) -> List:
+            acc = []
+            l = tuple([i for i in range(n)])
+            for i in l:
+                acc.append(i + 1)
+            return acc
+    """
+    with self.in_module(codestr) as mod:
+        f = mod.f
+        self.assertNotInBytecode(f, "FOR_ITER")
+        self.assertEqual(f(4), [i + 1 for i in range(4)])
---
+++ skipped_tests/test_invoke_with_cell.py
@@ -0,0 +1,13 @@
+# Reason: Code hitted some skipped words
+def test_invoke_with_cell(self):
+    codestr = """
+        def f(l: list):
+            x = 2
+            return [x + y for y in l]
+        def g():
+            return f([1,2,3])
+    """
+    with self.in_strict_module(codestr) as mod:
+        g = mod.g
+        self.assertEqual(g(), [3, 4, 5])
+        self.assertInBytecode(g, "INVOKE_FUNCTION", ((mod.__name__, "f"), 1))
