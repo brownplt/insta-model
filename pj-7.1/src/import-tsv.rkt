5992365526678c776e8ec4538251fecd01eca3d1
Ben Greenman <benjaminlgreenman@gmail.com>
2022-01-15 01:27:46 -0500

    pj: appendix, import table

---
+++ pj-7.1/src/import-tsv.rkt
@@ -0,0 +1,122 @@
+#lang racket/base
+
+(require
+  racket/list
+  racket/string)
+
+(define (parse tsv)
+  (with-input-from-file
+    tsv
+    (lambda ()
+      (for/list ((ln (in-lines)))
+        (tab-split ln)))))
+
+(define (tab-split str)
+  (string-split str "\t"))
+
+(define (comma-split str)
+  (string-split str ","))
+
+(define (space-join str*)
+  (string-join str* " "))
+
+(define (print-tex v**)
+  (if (= 2 (length (car v**)))
+    (print2 v**)
+    (printN+ v**)))
+
+(define (print2 v**)
+  (printf "\\begin{tabular}{ll}~n")
+  (print-first-row (map colname (car v**)))
+  (for ((v* (in-list (cdr v**))))
+    (define-values [tt0 tt1] (format-title2 (car v*)))
+    (define-values [cm0 cm1]
+      (let ((cm (cadr v*))
+            (N (string-length "time ./python -X jit -X jit-list-file=Tools/benchmarks/jitlist_richards_static.txt -X jit-enable-jit-list-wildcards")))
+        (if (< (string-length cm) N)
+          (values (format-cmd cm) "")
+          (values (format-cmd (substring cm 0 N))
+                  (format-cmd (substring cm N))))))
+    (print-row (list tt0 cm0))
+    (print-row (list tt1 cm1))
+    (void))
+  (printf "\\end{tabular}~n")
+  (void))
+
+(define (printN+ v**)
+  (if (<= (length (car v**)) 15)
+    (printN v**)
+    (let ()
+      (define-values [fst* rst*]
+        (for/lists (_1 _2)
+                   ((v* (in-list v**)))
+          (split-at v* 15)))
+      (printN fst*)
+      (newline)
+      (printN+ rst*))))
+
+(define (printN v**)
+  (printf "\\begin{tabular}{~a}~n" (make-string (length (car v**)) #\r))
+  (define-values [fst* snd*]
+    (for/lists (_1 _2)
+               ((v (in-list (car v**))))
+      (format-title2 v)))
+  (print-row fst*)
+  (print-first-row snd*)
+  (for ((v* (in-list (cdr v**))))
+    (print-row (map format-num v*)))
+  (printf "\\end{tabular}~n")
+  (void))
+
+(define (colname str)
+  (format "\\colname{~a}" str))
+
+(define (format-title str)
+  (define-values [kind bm opt*]
+    (let* ((m (regexp-match #rx"^([^ ]+) ([^(]*)\\(([^)]*)\\)$" str)))
+      (if m
+        (values (cadr m)
+                (let ((s (string-trim (caddr m))))
+                  (and (< 0 (string-length s)) s))
+                (map string-trim (comma-split (cadddr m))))
+        (raise-arguments-error 'format-title "failed to parse" "str" str))))
+  (define k+ (colname (simpl kind)))
+  (define o+ (space-join (map colname opt*)))
+  (if bm
+    (format "~a ~a (~a)" bm k+ o+)
+    (format "~a (~a)" k+ o+)))
+
+(define (format-title2 str)
+      (let ((s* (string-split (format-title str) " (")))
+        (values (car s*) (string-append "(" (cadr s*)))))
+
+(define (format-cmd str)
+  (format "\\texttt{~a}" (tex-escape str)))
+
+(define (tex-escape str)
+  (string-replace str "_" "\\_"))
+
+(define (format-num str)
+  (format "$~a$" str))
+
+(define (simpl str)
+  (case str
+    (("Typed") "T-Max")
+    (("TypedBasic") "T-Min")
+    (("TypedBasic2") "T-Min2")
+    (("Original") "Orig")
+    (else (raise-argument-error 'simpl "Carl-name" str))))
+
+(define (print-row str* [extra-tail ""])
+  (displayln (string-append "  " (string-join str* " & ") " \\\\" extra-tail)))
+
+(define (print-first-row str*)
+  (print-row str* "\\hline"))
+
+(module+
+  main
+  (require racket/cmdline)
+  (command-line
+    #:program "import-tsv"
+    #:args (tsv)
+    (print-tex (parse tsv))))
